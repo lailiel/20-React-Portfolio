d3f46eebfcc20c95522fe40658b9a1194b36577b
Abi L <124858748+lailiel@users.noreply.github.com>
2023-08-03 20:39:27 -0500

    fixed footplacement

--- react-portfolio/src/components/Footer.jsx
+++ react-portfolio/src/components/Footer.jsx
@@ -5,16 +5,14 @@ import Container from 'react-bootstrap/Container'
 export default function FooterComponent() {
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
-    <footer className='bg-light text-center text-white absolute-bottom'>
-      <Container >
-        <section classsName='mb-2 '>
+    <footer className='bg-light text-center text-white footer'>
+      <Container classsName='mb-2 '>
           <a href='https://github.com/lailiel'>
            <img id='icon' src={githubLogo} className='m-4'style={{width:'30px', height:'30px'}} ></img>
           </a>
           <a href='https://www.linkedin.com/in/abilivick/'>
           <img id='icon' src={linkedinLogo} className='m-4'style={{width:'30px', height:'30px'}} ></img>
           </a>
-        </section>
       </Container>
     </footer>
 
