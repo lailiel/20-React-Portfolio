34e367d526095511e1d765667658791c59883bea
Abi L <124858748+lailiel@users.noreply.github.com>
2023-08-06 13:41:02 -0500

    adjusted navbar responsiveness

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,17 @@
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import name from '../assets/Name-Color-Linear.svg'
-import Logo from '../assets/Logo-Color.svg'
+import logo from '../assets/Logo-Color.svg'
 
 
 export default function CustomNavbar() {
   return (
     <>
-    <Navbar data-bs-theme="dark" expand="sm" style={{height: '90px'}}className='sticky-top' id="navbar">
+    <Navbar data-bs-theme="dark" expand="md" style={{height: '90px'}}className='sticky-top' id="navbar">
       <Container>
-        <Navbar.Brand href="/" id='nav-name'>
-        {/* <img src={Logo} style={{height: '40px'}}/> */}
-          <img src={name} style={{height: '40px'}}/>
+        <Navbar.Brand href="/" id='nav-name' className='p-3'>
+          <img id="nav-default" src={name} style={{height: '40px'}}/>
+          <img id="nav-logo" src={logo} style={{height: '40px'}}/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
